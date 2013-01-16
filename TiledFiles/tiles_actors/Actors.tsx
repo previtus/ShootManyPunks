1af41ebe638fef71e41171773d3babb4c13c3e34
Bohuslav Machac <bogo777@gmail.com>
2013-01-17 00:52:03 +0100

    priprava nacitani postav z tmx souboru

---
+++ TiledFiles/tiles_actors/Actors.tsx
@@ -0,0 +1,24 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="Actors" tilewidth="64" tileheight="64">
+ <image source="actors.png" trans="ffffff" width="256" height="64"/>
+ <tile id="0">
+  <properties>
+   <property name="actor" value="EnemyDude"/>
+  </properties>
+ </tile>
+ <tile id="1">
+  <properties>
+   <property name="actor" value="EnemyDudeMelee"/>
+  </properties>
+ </tile>
+ <tile id="2">
+  <properties>
+   <property name="actor" value="QuestGiver"/>
+  </properties>
+ </tile>
+ <tile id="3">
+  <properties>
+   <property name="actor" value="npc_simple"/>
+  </properties>
+ </tile>
+</tileset>
