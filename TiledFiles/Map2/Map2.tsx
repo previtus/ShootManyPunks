32050c7280efcf1733297170f3b4690eb11cb016
Bohuslav Machac <bogo777@gmail.com>
2013-01-17 17:16:52 +0100

    tileset pro druhý level

---
+++ TiledFiles/Map2/Map2.tsx
@@ -0,0 +1,109 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="Map2" tilewidth="40" tileheight="40">
+ <image source="Sheet_001.png" trans="000000" width="160" height="480"/>
+ <tile id="15">
+  <properties>
+   <property name="Solid" value="1"/>
+  </properties>
+ </tile>
+ <tile id="16">
+  <properties>
+   <property name="Solid" value="1"/>
+  </properties>
+ </tile>
+ <tile id="17">
+  <properties>
+   <property name="Solid" value="1"/>
+  </properties>
+ </tile>
+ <tile id="18">
+  <properties>
+   <property name="Solid" value="1"/>
+  </properties>
+ </tile>
+ <tile id="19">
+  <properties>
+   <property name="Solid" value="1"/>
+  </properties>
+ </tile>
+ <tile id="20">
+  <properties>
+   <property name="Solid" value="1"/>
+  </properties>
+ </tile>
+ <tile id="21">
+  <properties>
+   <property name="Solid" value="1"/>
+  </properties>
+ </tile>
+ <tile id="22">
+  <properties>
+   <property name="Solid" value="1"/>
+  </properties>
+ </tile>
+ <tile id="23">
+  <properties>
+   <property name="Solid" value="1"/>
+  </properties>
+ </tile>
+ <tile id="24">
+  <properties>
+   <property name="Solid" value="1"/>
+  </properties>
+ </tile>
+ <tile id="25">
+  <properties>
+   <property name="Solid" value="1"/>
+  </properties>
+ </tile>
+ <tile id="26">
+  <properties>
+   <property name="Solid" value="1"/>
+  </properties>
+ </tile>
+ <tile id="27">
+  <properties>
+   <property name="Solid" value="1"/>
+  </properties>
+ </tile>
+ <tile id="28">
+  <properties>
+   <property name="Solid" value="1"/>
+  </properties>
+ </tile>
+ <tile id="29">
+  <properties>
+   <property name="Solid" value="1"/>
+  </properties>
+ </tile>
+ <tile id="30">
+  <properties>
+   <property name="Solid" value="1"/>
+  </properties>
+ </tile>
+ <tile id="31">
+  <properties>
+   <property name="Solid" value="1"/>
+  </properties>
+ </tile>
+ <tile id="32">
+  <properties>
+   <property name="Solid" value="1"/>
+  </properties>
+ </tile>
+ <tile id="33">
+  <properties>
+   <property name="Solid" value="1"/>
+  </properties>
+ </tile>
+ <tile id="34">
+  <properties>
+   <property name="Solid" value="1"/>
+  </properties>
+ </tile>
+ <tile id="35">
+  <properties>
+   <property name="Solid" value="1"/>
+  </properties>
+ </tile>
+</tileset>
